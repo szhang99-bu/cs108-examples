7eb4d185120701011ef769e1af35bb1161dcf8a1
Shiyu Zhang <szhang99@bu.edu>
2021-03-24 17:42:20 -0400

    Created show profile page feature

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,8 +2,9 @@
 # description: direct URL requests to view functions
 
 from django.urls import path
-from .views import ShowAllProfilesView
+from .views import ShowAllProfilesView, ShowProfilePageView
 
 urlpatterns = [
     path('',ShowAllProfilesView.as_view(), name = 'home_page'),
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name = 'show_profile_page'),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
 # Create your views here.
@@ -11,3 +11,11 @@ class ShowAllProfilesView(ListView):
     model = Profile #restieve Quote objects from teh database
     template_name = "mini_fb/show_all_profiles.html"
     context_object_name = "profiles"
+
+class ShowProfilePageView(DetailView):
+    """
+        Display single quote object.
+    """
+    model = Profile #restieve Profile objects from the database
+    template_name = "mini_fb/show_profile_page.html" #delagate the display to this template
+    context_object_name = "profiles" # use this variable name in the template
