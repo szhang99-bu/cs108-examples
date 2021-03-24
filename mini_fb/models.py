a1e8565bf9a8d1e9ac26addb13a1b8b12f133f9b
Shiyu Zhang <szhang99@bu.edu>
2021-03-24 18:10:22 -0400

    Created StatusMessage model, show status messages on profile page

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -17,3 +17,25 @@ class Profile(models.Model):
             Return a string representation of the name city and email
         """
         return f'{self.first_name} {self.last_name} {self.city} {self.email}'
+    
+    def get_status_messages(self):
+        """
+            Return all message for this Person.
+        """
+
+        #use the object manager to filter Quotes by this person's pk:
+        return StatusMessage.objects.filter(profile=self)
+
+class StatusMessage(models.Model):
+    """
+        model called StatusMessage, which will model the data attributes of Facebook status message.
+    """
+    timestamp = models.TextField(blank=True)
+    message = models.TextField(blank=True)
+    profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        """
+            Return a string reoresentation of this quote.
+        """
+        return f'{self.message} @ {self.timestamp}'
